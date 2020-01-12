73f8a6871569015162cc4edb3dc974c201c70fd3
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-01-12 01:11:42 +0100

    ne

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ let express = require("express");
 let fs = require("fs");
 let app = express();
 let genid = require("uuid");
-let session = require("express-session");
 let createFolders = require("./process/createfolders");
 let createImages = require("./process/createimages");
 let writeManifest = require("./process/writeManifest");
 let zipFile = require("./process/zipFile");
 let delDocs = require("./process/delDocs");
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    cookie: { secure: true }
-  })
-);
 
 app.use(createFolders, createImages, writeManifest, zipFile, delDocs);
 
